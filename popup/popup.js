97ed09e0f2859a777fa0d0e677c44645febb6f1d
kkleindev <kleinkevin.lu@gmail.com>
2017-12-10 21:24:28 +0100

    Make value display consider time spent on currently active website.

--- popup/popup.js
+++ popup/popup.js
@@ -7,16 +7,26 @@ function listenForClicks() {
 
     function updateTime(storage) {
       const button = document.getElementById("popup-btn");
-      if (button) {
-        // TODO(kkleindev): Isolate/modularize time conversion.
-        const wastedSeconds = Math.round(storage.timeCount / 60000 * 60);
-        const wastedMoney =  wastedSeconds * storage.wage / 3600
-        button.textContent = wastedMoney.toFixed(2).toString();
-        // TODO(kkleindev): This actually removes the the properties to be
-        // associated with the button, i.e. pointer cursor and background
-        // change. This is counterintuitive and hacky.
-        button.setAttribute("class", "button");
+      if (!button) {
+        return;
       }
+      // If current website is desirable, timeCount already stores the
+      // sought-after value. If not, we need to add the difference between now
+      // and start time.
+      let wastedSeconds = 0;
+      if (!storage.currentIsDesirable){
+        const date = new Date();
+        wastedSeconds += date.getTime() - storage.startTime;
+      }
+      wastedSeconds += storage.timeCount;
+      // TODO(kkleindev): Isolate/modularize time conversion.
+      wastedSeconds = Math.round(wastedSeconds / 60000 * 60);
+      const wastedMoney =  wastedSeconds * storage.wage / 3600
+      button.textContent = wastedMoney.toFixed(2).toString();
+      // TODO(kkleindev): This actually removes the the properties to be
+      // associated with the button, i.e. pointer cursor and background
+      // change. This is counterintuitive and hacky.
+      button.setAttribute("class", "button");
     }
 
     const storagePromise = browser.storage.local.get();
