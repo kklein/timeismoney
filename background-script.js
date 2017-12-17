b3d0902528b515a3eb82f7c9caf34be96591eee3
kkleindev <kleinkevin.lu@gmail.com>
2017-12-17 23:26:52 +0100

    Store timeCounts in a per calendarweek fashion instead of a simple total count.

--- background-script.js
+++ background-script.js
@@ -1,11 +1,40 @@
+function getWeekId(date) {
+    const dateCopy = new Date(date.valueOf());
+
+    // ISO weeks start on Mondays.
+    const dayNumber = (date.getDay() + 6) % 7;
+
+    // Set the target to the Thursday of this week so the
+    // target date is in the right year.
+    dateCopy.setDate(dateCopy.getDate() - dayNumber + 3);
+
+    // ISO 8601: Week 1 iff January 4th in it.
+    const dateJan4 = new Date(dateCopy.getFullYear(), 0, 4);
+
+    // Number of days between target date and january 4th
+    const dayDiff = (dateCopy - dateJan4) / 86400000;
+
+    // Calculate week number: Week 1 (january 4th) plus the
+    // number of weeks between target date and january 4th
+    const weekId = dateCopy.getFullYear().toString() +
+        (1 + Math.ceil(dayDiff / 7)).toString();
+    return parseInt(weekId);
+}
+
 function initializeState(storageData) {
+  const date = new Date();
+  const weekId = getWeekId(date);
+  const defaultTimeCount = {};
+  defaultTimeCount[weekId] = 0;
   chrome.storage.local.set({
     currentIsDesirable: true,
-    timeCount: storageData.timeCount ? storageData.timeCount : 0,
+    timeCount:
+        storageData.timeCount ? storageData.timeCount : defaultTimeCount,
     // TODO(kkleindev): nicer constant declaration.
     wage: storageData.wage ? storageData.wage : 10,
     // TODO(kkleindev): nicer constant declaration.
-    websites: storageData.websites ? storageData.websites : "www.facebook.com"
+    websites: storageData.websites ?
+        storageData.websites : ["www.facebook.com"]
   });
 }
 
@@ -21,10 +50,12 @@ function updateState(tabData, storageData) {
   const urlIsDesirable = isDesirable(url, storageData.websites);
   const date = new Date();
   if (currentIsDesirable && !urlIsDesirable) {
-    chrome.storage.local.set({currentIsDesirable: false,
-        timeCount: timeCount, startTime: date.getTime()});
+    chrome.storage.local.set(
+        {currentIsDesirable: false, startTime: date.getTime()});
   } else if (!currentIsDesirable && urlIsDesirable) {
-    const newTimeCount = timeCount + date.getTime() - startTime;
+    const weekId = getWeekId(date);
+    const newTimeCount = timeCount;
+    newTimeCount[weekId] += date.getTime() - startTime;
     chrome.storage.local.set(
         {currentIsDesirable: true, timeCount: newTimeCount});
   }
@@ -35,7 +66,9 @@ function finalizeState(storageData) {
   if (!storageData.currentIsDesirable) {
     const timeCount = storageData.timeCount;
     const startTime = storageData.startTime;
-    const newTimeCount = timeCount + date.getTime() - startTime;
+    const weekId = getWeekId(date);
+    const newTimeCount = timeCount;
+    timeCount[weekId] += date.getTime() - startTime;
     chrome.storage.local.set(
         {currentIsDesirable: true, timeCount: newTimeCount});
     console.log('stopped counting.');
--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,28 @@
+// Courtesy: https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php/6117889#6117889
+
+function getWeekId(date) {
+  const dateCopy = new Date(date.valueOf());
+
+  // ISO weeks start on Mondays.
+  const dayNumber = (date.getDay() + 6) % 7;
+
+  // Set the target to the Thursday of this week so the
+  // target date is in the right year.
+  dateCopy.setDate(dateCopy.getDate() - dayNumber + 3);
+
+  // ISO 8601: Week 1 iff January 4th in it.
+  const dateJan4 = new Date(dateCopy.getFullYear(), 0, 4);
+
+  // Number of days between target date and january 4th
+  const dayDiff = (dateCopy - dateJan4) / 86400000;
+
+  // Calculate week number: Week 1 (january 4th) plus the
+  // number of weeks between target date and january 4th
+  const weekId = dateCopy.getFullYear().toString() +
+      (1 + Math.ceil(dayDiff / 7)).toString();
+  return parseInt(weekId);
+}
+
 function displaySum(storageData) {
   const button = document.getElementById("popup-btn");
   if (!button) {
@@ -7,11 +32,11 @@ function displaySum(storageData) {
   // sought-after value. If not, we need to add the difference between now
   // and start time.
   let wastedSeconds = 0;
+  const date = new Date();
   if (!storageData.currentIsDesirable){
-    const date = new Date();
     wastedSeconds += date.getTime() - storageData.startTime;
   }
-  wastedSeconds += storageData.timeCount;
+  wastedSeconds += storageData.timeCount[getWeekId(date)];
   // TODO(kkleindev): Isolate/modularize time conversion.
   wastedSeconds = Math.round(wastedSeconds / 60000 * 60);
   const wastedMoney =  wastedSeconds * storageData.wage / 3600
